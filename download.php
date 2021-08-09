3b1152c14ead2c5f056c13c51a902f8eec0fe4d0
Dexter666888 <87178786+Dexter666888@users.noreply.github.com>
2021-08-09 16:17:24 +0800

    Add files via upload

---
+++ download.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DownloadMinecraftBedrock</title>
+</head>
+<body>
+    <div align="center" style="color: black;">
+        <h1>DownloadMinecraftBedrock</h1>
+        <hr>
+        </br>
+        </br>
+        </br>
+        </br>
+        <a href="MCBRK/minecraft-1-17-10.apk"></a>
+        <?php
+            $x=$_FILES;
+            echo $x;
+        ?>
+    </div>
+</body>
+</html>
